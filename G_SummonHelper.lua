2e7b9c0e38ecbd033004dee7609c2990ffba1319
PluieYu <yuyoubiyi@hotmail.com>
2024-05-27 00:08:00 +0200

    V1.2 update

--- G_SummonHelper.lua
+++ G_SummonHelper.lua
@@ -8,11 +8,12 @@
 --main class--
 SummonHelper = AceLibrary("AceAddon-2.0"):new("AceEvent-2.0","AceModuleCore-2.0","AceComm-2.0","AceDB-2.0","AceDebug-2.0","AceConsole-2.0","FuBarPlugin-2.0")
 local L = AceLibrary("AceLocale-2.2"):new("SummonHelper")
-
+SpellStatus = AceLibrary("SpellStatus-1.0")
 local BS = AceLibrary("Babble-Spell-2.2")
-local SpellStatus = AceLibrary("SpellStatus-1.0")
 
 SummonHelper.hasIcon = "Interface\\Icons\\spell_shadow_twilight"
+SummonHelper.Prefix =
+"|cffF5F54A["..base64:dec("5bCP55qu566x").."]|r|cff9482C9"..base64:dec("5pyv5aOr5Yqp5omL").."|r"
 SummonHelper.defaultMinimapPosition = 160
 SummonHelper.hideWithoutStandby = true
 SummonHelper.options = {
@@ -34,30 +35,23 @@ SummonHelper.options = {
 		},
 	}
 }
-
-
-
 function SummonHelper:OnInitialize()
 	self.playerClass = UnitClass("player")
-	self.Prefix =
-	"|cffF5F54A["..base64:dec("5bCP55qu566x").."]|r|cff9482C9"..base64:dec("5pyv5aOr5Yqp5omL").."|r"
 	self:SetDebugLevel(3)
 	self.SummonList = {}
-	self.SummonListHide = {}
 	self:RegisterDB("SummonHelperDB")
 	self:RegisterDefaults("profile", {
-		xOfs = nil,
-		yOfs = nil,
-		point = nil,
-		relativePoint = nil,
+		xOfs = 0,
+		yOfs = 0,
+		point = "center",
+		relativePoint = "center",
 	})
 	self.opt = self.db.profile
-	self.SummonHelperFrame = SummonHelperFrame
-	self.SummonHelperFrame:SetupFrame()
+	SHMain:OnInitialize()
 	self.OnMenuRequest = SummonHelper.options
 	self:SetCommPrefix(self.Prefix)
 	self:RegisterChatCommand({"/smh", "/SummonHelper"}, SummonHelper.options)
-	DEFAULT_CHAT_FRAME:AddMessage(self:BuildMessage(L["已加载"]))
+	DEFAULT_CHAT_FRAME:AddMessage(buildMessage(L["已加载"]))
 end
 
 function SummonHelper:OnProfileEnable()
@@ -69,6 +63,7 @@ function SummonHelper:OnEnable()
 		SummonHelper:OnDisable()
 		return
 	end
+
 	self:RegisterComm(self.Prefix, "RAID")
 	if not self:IsCommRegistered(self.Prefix,"RAID") then
 		self:RegisterComm(self.Prefix, "RAID")
@@ -81,225 +76,127 @@ function SummonHelper:OnEnable()
 	self:RegisterEvent("CHAT_MSG_RAID_LEADER", "CheckChatMessage")
 	self:RegisterEvent("CHAT_MSG_WHISPER", "CheckChatMessage")
 
-	self:RegisterEvent("SpellStatus_SpellCastCastingStart")
+	--self:RegisterEvent("SpellStatus_SpellCastCastingStart")
 	self:RegisterEvent("SpellStatus_SpellCastFailure")
-	self:RegisterEvent("SpellStatus_SpellCastChannelingStart")
+	--self:RegisterEvent("SpellStatus_SpellCastChannelingStart")
 	self:RegisterEvent("SpellStatus_SpellCastChannelingFinish")
-
 end
-
 function SummonHelper:OnDisable()
-	if self:IsCommRegistered(self.Prefix,"RAID") then
-		SummonHelper:UnregisterComm(self.Prefix, "RAID")
-	end
 	SummonHelper:UnregisterAllEvents()
-	self.SummonHelperFrame.frame:Hide()
-end
-
-
-function SummonHelper:BuildMessage(TEXT, args1, args2 )
-	local subMsg
-	if args2 then
-		 subMsg = format(TEXT, args1, args2)
-	elseif args1 then
-		 subMsg = format(TEXT, args1)
-	else
-		subMsg = TEXT
+	if SHMain.mf then
+		SHMain.mf:Hide()
 	end
-	return format("%s %s", self.Prefix, subMsg)
 end
+
 
 function SummonHelper:CheckChatMessage(msg, name)
 	local stringLen = string.len(self.Prefix)
+	local SLLen = tLength(self.SummonList)
 	if strsub(msg,0, stringLen) ~= self.Prefix then
-		for _, word in pairs(L["关键词"]) do
-			index_stat, index_end = string.find(msg, word);
-			if index_stat then
-				self:OnCommReceive("", "", "RAID", "Add", name)
-				self:SendCommMessage("RAID","Add",name)
-				self.SummonHelperFrame.frame:Show()
+		if tContain(self.SummonList, name) then
+		else
+			for _, word in pairs(L["关键词"]) do
+				index_stat, index_end = string.find(msg, word);
+				if index_stat then
+					addT(self.SummonList, name)
+					SHMain:Flush()
+				end
 			end
 		end
+		SendChatMessage(buildMessage(L["你已在队列中" ], tostring(SLLen)), "WHISPER", nil, name)
 	end
 end
-
-function SummonHelper:SpellStatus_SpellCastCastingStart(_, name, _, _, _, _)
-	--id, name, fullName, startTime, stopTime, duration
-	if name == BS["Ritual of Summoning"] then
-		self:LevelDebug(2, format("SpellStatus_SpellCastCastingStart: <%s>", tostring(name)))
-		self.SummoningTarget = UnitName("target")
-		self:SendCommMessage("RAID","Summoning", self.SummoningTarget )
-	end
-end
-function SummonHelper:SpellStatus_SpellCastChannelingStart(_, name, _, _, _, _)
-	--id, name, fullName, startTime, stopTime, duration
-	if name == BS["Ritual of Summoning"] then
-		self:LevelDebug(2, format("SpellStatus_SpellCastChannelingStart: <%s>", tostring(name)))
-		SendChatMessage(self:BuildMessage(L["召唤仪式已经启动"]), "WHISPER", nil, self.SummoningTarget)
-
-	end
-end
-
-function SummonHelper:SpellStatus_SpellCastFailure(_, name, _, raison, _, _, _)
-	--id, name, fullName, raison,raison2,raison3,raison4
-	if name == BS["Ritual of Summoning"] then
-		self:LevelDebug(2, format("SpellStatus_SpellCastFailure on : <%s> for the reason <%s>", tostring(name), tostring(raison)))
-		self:SendCommMessage("RAID","SummoningFailure", self.SummoningTarget )
-		SendChatMessage(self:BuildMessage(L["召唤仪式遭到破坏"]), "WHISPER", nil, self.SummoningTarget)
-	end
-end
-
-
-function SummonHelper:SpellStatus_SpellCastChannelingFinish(_, name, _, raison)
-	--id, name, fullName, raison
-	if name == BS["Ritual of Summoning"] then
-		self:LevelDebug(2, format("SpellStatus_SpellCastChannelingFinish on : <%s> for the reason <%s>", tostring(name), tostring(raison)))
-		SendChatMessage(self:BuildMessage(L["召唤仪式已经完成"]), "WHISPER", nil, self.SummoningTarget)
-		SummonHelper:RemoveFonc(self.SummoningTarget)
-
-	end
-end
-
-
-function SummonHelper:OnCommReceive(_, _, _, method, target)
+function SummonHelper:OnCommReceive(_, sender, _, method, target)
 	--prefix, sender, distribution, method, target
-	if target ~= nil and method ~= nil then
-		targetPos = tablefind(self.SummonList, target)
-		self:LevelDebug(2, format("Got Comm Msg: <%s> on <%s>", tostring(method), tostring(target)))
-		if method=="Add"  and targetPos == nil then
-			table.insert(self.SummonList, target)
-			self:Reflash()
-		elseif method=="Remove" and targetPos ~= nil then
-			table.remove(self.SummonList, targetPos)
-			collectgarbage()
-			self:Reflash()
-		elseif method=="Summoning" and targetPos ~= nil then
-			self:HideSummoningTarget(target)
-		elseif method=="SummoningFailure" and targetPos ~= nil then
-			self:ShowSummoningTarget(target)
+	if target ~= nil and method ~= nil  and sender~="player"  then
+		if method=="Add" then
+			addT(self.SummonList, target)
+		elseif method=="Remove" then
+			removeT(self.SummonList, target)
 		end
+		SHMain:Flush()
+		collectgarbage()
 	end
 end
-
-function tablefind(tab,el)
-	for index, value in pairs(tab) do
-			if value == el then
-				return index
-			end
-		end
+function SummonHelper:AddFonc(name)
+	if  tContain(self.SummonList, name) then
+		return
+	else
+		addT(self.SummonList, name)
+		SHMain:Flush()
+		SummonHelper:SendCommMessage("RAID","Add", name )
 	end
-
+end
 function SummonHelper:RemoveFonc(name)
-	self:LevelDebug(2, format("RemoveFonc has been active on: %s", tostring(name)))
-	self:OnCommReceive("", "", "RAID", "Remove", name)
+	if tContain(self.SummonList, name) then
+		removeT(SummonHelper.SummonList, name)
+	end
+	SHMain:Flush()
 	SummonHelper:SendCommMessage("RAID","Remove", name )
 end
 function SummonHelper:SummonFonc(name)
 	self:LevelDebug(2, format("SummonFoncButton has been Clicked on: %s", tostring(name)))
-	SummonHelper:SendCommMessage("RAID","Summoning", name )
-	local unitId = self:GetUnitId(name)
-	local playerZone = self:GetZone("player")
+	local chatType = GetNumRaidMembers() > 0 and "RAID" or "PARTY"
+	local unitId = getRaidIndex(name)
+	local nameC = getUnitNameWithColors(name)
+	local playerZone = GetZoneText()..GetSubZoneText()
 	TargetUnit(unitId)
 	if UnitAffectingCombat("player") then
 		self:Print(L["你正在战斗中"])
 	elseif UnitAffectingCombat(unitId) then
-		self:Print(string.format(L["%s 在战斗中"], name))
+		SendChatMessage(buildMessage(L["%s 在战斗中"],nameC), chatType)
 	elseif CheckInteractDistance("target", 4) then
-		self:Print(string.format(L["%s 已在身边"], name))
+		self:Print(buildMessage(L["%s 已在身边"], name))
 		self:RemoveFonc(name)
 	else
 		--CastSpellByName(BS["Ritual of Summoning"])
-		SpellStatus:CastSpellByName(BS["Ritual of Summoning"],false)
-
-		local chatType = "PARTY"
-		if GetNumRaidMembers() > 0 then
-			chatType = "RAID"
-		end
-		SendChatMessage(self:BuildMessage(L["正在将%s拉到 %s"],self:GetUnitNameWithColors(name), playerZone), chatType)
-		SendChatMessage(self:BuildMessage(L["正在召唤你倒 %s"], playerZone), "WHISPER", nil, name)
+		SpellStatus:CastSpellByName(BS["Ritual of Summoning"], false)
+		SendChatMessage(buildMessage(L["正在将%s拉到 %s"],nameC, playerZone), chatType)
+		SendChatMessage(buildMessage(L["正在召唤你倒 %s"], playerZone), "WHISPER", nil, name)
+		self:RemoveFonc(name)
 	end
 end
-
-function SummonHelper:GetUnitId(name)
-	if GetNumRaidMembers() > 0 then
-		for i = 1, GetNumRaidMembers(), 1 do
-			local aName = GetRaidRosterInfo(i)
-			if aName == name then
-				return "Raid" .. i
-			end
-		end
-	elseif GetNumPartyMembers() > 0 then
-		for i = 1, GetNumPartyMembers(), 1 do
-			local unitId = "Party" .. i
-			local aName = UnitName(unitId)
-			if aName == name then
-				return unitId
-			end
-		end
-	end
-	return nil
-end
-function SummonHelper:GetZone(unitId)
-	if GetNumRaidMembers() > 0 then
-		if unitId then
-			if unitId == "player" then
-				unitId = self:GetUnitId(GetUnitName(unitId))
-				local _, _, _, _, _, _, zone = GetRaidRosterInfo(string.sub(unitId, 5))
-				return zone
-			elseif string.find(unitId, "Raid") then
-				local _, _, _, _, _, _, zone = GetRaidRosterInfo(string.sub(unitId, 5))
-				return zone
-			end
-		end
-	elseif GetNumPartyMembers() > 0 then
-		if unitId then
-			-- how do I get the zone of party members?
-			return GetRealZoneText()
-		end
-	end
-
-	return nil
-end
-function SummonHelper:Reflash()
-	for i = 1, 5 do
-		if self.SummonList[i] then
-			if tablefind(self.SummonListHide, self.SummonList[i]) then
-				self:LevelDebug(2, format("Target in the hiden list: %s", tostring(tablefind(self.SummonListHide, self.SummonList[i]) )))
-			end
-			if not tablefind(self.SummonListHide, self.SummonList[i]) then
-				self.SummonHelperFrame.frame.Candidate["player"..i].text:SetText(self.SummonList[i])
-				self.SummonHelperFrame.frame.Candidate["player"..i]:Show()
-			end
-		else
-			self.SummonHelperFrame.frame.Candidate["player"..i].text:SetText("N/A")
-			self.SummonHelperFrame.frame.Candidate["player"..i]:Hide()			end
-		end
-end
-function SummonHelper:HideSummoningTarget(target)
-	for i = 1, 5 do
-		if self.SummonHelperFrame.frame.Candidate["player"..i].text:GetText()  == target then
-			self:LevelDebug(2, format("HideSummoningTarget: %s", tostring(target)))
-			self.SummonHelperFrame.frame.Candidate["player"..i]:Hide()
-			table.insert(self.SummonListHide , target)
-			break
-		end
+--function SummonHelper:SpellStatus_SpellCastChannelingStart(_, name, _, _, _, _)
+--	--id, name, fullName, startTime, stopTime, duration
+--	if name == BS["Ritual of Summoning"] then
+--		SendChatMessage(buildMessage(L["召唤仪式已经启动"]), "WHISPER", nil, SHMain.currentT)
+--		--SummonHelper:RemoveFonc(SHMain.currentT)
+--	end
+--end
+function SummonHelper:SpellStatus_SpellCastFailure(_, name, _, raison, _, _, _)
+	--id, name, fullName, raison,raison2,raison3,raison4
+	if name == BS["Ritual of Summoning"] then
+		SendChatMessage(buildMessage(L["召唤仪式遭到破坏"]), "WHISPER", nil, SHMain.currentT)
+		SummonHelper:AddFonc(SHMain.currentT)
 	end
 end
-function SummonHelper:ShowSummoningTarget(target)
-	for i = 1, 5 do
-		if self.SummonHelperFrame.frame.Candidate["player"..i].text:GetText()  == target then
-			self:LevelDebug(2, format("ShowSummoningTarget: %s", tostring(target)))
-			self.SummonHelperFrame.frame.Candidate["player"..i]:Show()
-			targetPos = tablefind(self.SummonListHide, target)
-			if targetPos then
-				table.remove(self.SummonListHide, targetPos)
-			end
-			break
-		end
+function SummonHelper:SpellStatus_SpellCastChannelingFinish(_, name, _, raison)
+	--id, name, fullName, raison
+	if name == BS["Ritual of Summoning"] then
+		SendChatMessage(buildMessage(L["召唤仪式已经完成"]), "WHISPER", nil, SHMain.currentT)
 	end
 end
-
-function SummonHelper:GetRaidIndex(name)
+function buildMessage(TEXT, args1, args2 )
+	local subMsg
+	if args2 then
+		subMsg = format(TEXT, args1, args2)
+	elseif args1 then
+		subMsg = format(TEXT, args1)
+	else
+		subMsg = TEXT
+	end
+	return format("%s %s", SummonHelper.Prefix, subMsg)
+end
+function getUnitNameWithColors(name)
+	local raidIndex = getRaidIndex(name)
+	--local UnitId = getUnitId(name)
+	local _, classFilename = UnitClass(raidIndex)
+	local c = RAID_CLASS_COLORS[classFilename]
+	local classhexe = string.format("%2x%2x%2x", c.r*255, c.g*255, c.b*255)
+	local nameWithColors = string.format("|cFF%s%s|r",  classhexe, name)
+	return nameWithColors
+end
+function getRaidIndex(name)
 	local prefix
 	local NumMembers = GetNumRaidMembers()
 	if NumMembers then
@@ -315,12 +212,36 @@ function SummonHelper:GetRaidIndex(name)
 		end
 	end
 end
-
-function SummonHelper:GetUnitNameWithColors(name)
-	local raidIndex = self:GetRaidIndex(name)
-	local _, classFilename = UnitClass(raidIndex)
-	local c = RAID_CLASS_COLORS[classFilename]
-	local classhexe = string.format("%2x%2x%2x", c.r*255, c.g*255, c.b*255)
-	local nameWithColors = string.format("|cFF%s%s|r",  classhexe, name)
-	return nameWithColors
+function addT(t, target)
+	local tLen = tLength(t)
+	table.insert(t,tLen + 1, target)
+end
+function removeT(t, target)
+	local p = tPos(t, target)
+	if p then
+		table.remove(t, p)
+	end
+end
+function tPos(t, target)
+	for i, v in ipairs(t) do
+		if v == target then
+			return i
+		end
+	end
+end
+function tLength(table)
+	local size = 0
+	if  table then
+		for i, v in pairs(table) do
+			size = size + 1
+		end
+	end
+	return size;
+end
+function tContain(t, target)
+	for _, v in ipairs(t) do
+		if v == target then
+			return true
+		end
+	end
 end
--- README.md
+++ README.md
@@ -5,16 +5,25 @@
 ### 或者已经安装了整合版的 !Libs
 
 下载 zip 格式文件
+
 下载地址 https://github.com/PluieYu/G_SummonHelper/archive/refs/heads/main.zip
 
 解压 G_SummonHelper-main.zip 到 interface/Addons/ 
+
 重命名 G_SummonHelper-main 到 G_SummonHelper 即可
 
 Locale 里的文字描述可以按照喜好随意修改 但一定要保留 %s
 
+### 更新日志
+#### 1.2 版本
+重构大部分代码 修复bug
+#### 1.1 版本
+支持团队协作 避免重复拉人
+#### 1.0 版本
+最多同时显示5个名单
+
 ### 使用方法
 团队 小队 密语发送 求拉 即可激活插件
+
 拉人界面将自动打开 也可以点击小地图按钮手动打开
 
-最多同时显示5个名单， 支持团队协作 正在拉的目标将对别的术士隐藏 已避免重复拉人
-团队协作需要插件版本大于1.1
